dd89df5c1124cab81bfacff4b0cf02250bcded2d
Toasterfire-come <carter.kiefer2010@outlook.com>
2024-12-21 11:50:10 -0600

    Update retrieve_data.py

--- blueprints/retrieve_data.py
+++ blueprints/retrieve_data.py
@@ -6,18 +6,9 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from threading import Event
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-while os.path.basename(current_dir) != "Stock-Scanner-Project":
-    current_dir = os.path.dirname(current_dir)
-    if current_dir == "/":
-        raise FileNotFoundError("Base directory 'Stock-Scanner-Project' not found!")
-
-base_dir = current_dir
-
 # Define the absolute path to the JSON file
-TICKER_FILE_PATH = os.path.join(base_dir, "json", "Sample_ticker.json")
-EXPORT_FILE_PATH = os.path.join(base_dir, "json", "stock_data_export.json")
+TICKER_FILE_PATH = json/stock_data_export.json
+EXPORT_FILE_PATH = json/Sample_tickers.json
 
 # Ensure the JSON file exists locally
 if not os.path.exists(TICKER_FILE_PATH):
