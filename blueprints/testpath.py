e66bf492955bc88a5ee44292c9e9dce4fb8c3a6b
Toasterfire-come <carter.kiefer2010@outlook.com>
2024-12-21 11:08:21 -0600

    Update testpath.py

--- blueprints/testpath.py
+++ blueprints/testpath.py
@@ -17,8 +17,6 @@ print("JSON file Found successfully!")
 
 except FileNotFoundError:
     print(f"Error: File not found at {json_file_path}")
-except json.JSONDecodeError as e:
-    print(f"Error decoding JSON: {e}")
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
 
