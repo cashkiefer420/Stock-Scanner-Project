df5a90e9dd0474c0cecf23b3e918e48693a89c0a
Toasterfire-come <carter.kiefer2010@outlook.com>
2024-12-23 14:03:19 -0600

    Create Retrieve.py

---
+++ blueprints/Retrieve.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+import json
+from datetime import datetime, timedelta
+import logging
+import os
+from concurrent.futures import ThreadPoolExecutor
+from threading import Event
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+while os.path.basename(current_dir) != "Stock-Scanner-Project":
+    current_dir = os.path.dirname(current_dir)
+    if current_dir == "/":
+        raise FileNotFoundError("Base directory 'Stock-Scanner-Project' not found!")
+
+base_dir = current_dir
+
+# Define the absolute path to the JSON file
+TICKER_FILE_PATH = os.path.join(base_dir, "json", "Sample_ticker.json")
+EXPORT_FILE_PATH = os.path.join(base_dir, "json", "stock_data_export.json")
+
+def load_tickers():
+    try:
+        with open(TICKER_FILE_PATH, 'r') as file:
+            data = json.load(file)
+            return data.get("tickers", [])
+            Print("tickers"
+
