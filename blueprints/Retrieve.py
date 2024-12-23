5cd7bcf2f2f5c2e5870d587f1c9060fca18e33ce
Toasterfire-come <carter.kiefer2010@outlook.com>
2024-12-23 14:11:20 -0600

    Update Retrieve.py

--- blueprints/Retrieve.py
+++ blueprints/Retrieve.py
@@ -24,4 +24,11 @@ def load_tickers():
         with open(TICKER_FILE_PATH, 'r') as file:
             data = json.load(file)
             return data.get("tickers", [])
-            Print("tickers"
+            Print("tickers")
+    except FileNotFoundError:
+        logging.error(f"Ticker File MIA")
+        return []
+    except json.JSONDecodeError:
+        logging.error("Json Error")
+        return []
+        
