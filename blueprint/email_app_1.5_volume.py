379778c769bc617a59b4e6fa307f5ed944fc786a
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-01-13 09:32:55 -0600

    Update email_app_1.5_volume.py

--- blueprint/email_app_1.5_volume.py
+++ blueprint/email_app_1.5_volume.py
@@ -20,9 +20,9 @@ SENDER_PASSWORD = 'pIqvin-persi2-pibsij'
 
 # Paths to JSON files
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-JSON_FILE = os.path.join(BASE_DIR, 'json', '100_volume.json')
-STOCK_INFO_FILE = os.path.join(BASE_DIR, 'json', 'Filtered_volume_100.json')
-USED_TICKERS_FILE = os.path.join(BASE_DIR, 'json', 'ut_volume_100.json')
+JSON_FILE = os.path.join(BASE_DIR, 'json', '1.5_volume.json')
+STOCK_INFO_FILE = os.path.join(BASE_DIR, 'json', 'Filtered_volume_1.5.json')
+USED_TICKERS_FILE = os.path.join(BASE_DIR, 'json', 'ut_volume_1.5.json')
 
 # Ensure JSON files exist
 for file in [JSON_FILE, USED_TICKERS_FILE]:
@@ -36,7 +36,7 @@ def is_valid_email(email):
     return re.match(regex, email)
 
 # Route to handle email subscription
-@app.route('/subscribe-volume-100-td', methods=['POST'])
+@app.route('/subscribe-volume-1.5-td', methods=['POST'])
 def subscribe_email():
     try:
         data = request.get_json()
@@ -80,7 +80,7 @@ def send_stock_notifications():
         html_template = """
         <html>
           <body>
-            <h1>Stock <strong>{{ stock_symbol }}</strong> One Hundred Million Volume Notification</h1>
+            <h1>Stock <strong>{{ stock_symbol }}</strong> 1.5 High Volume Notification</h1>
             <p>Dear Investor,</p>
             <p>The stock <strong>{{ stock_symbol }}</strong> is experiencing large volume.</p>
             <p>Current Price: ${{ current_price }}</p>
