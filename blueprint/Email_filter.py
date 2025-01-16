7726d8121baf6c12720e447e605192eb43e0213d
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-01-15 20:17:13 -0600

    Update Email_filter.py

--- blueprint/Email_filter.py
+++ blueprint/Email_filter.py
@@ -55,9 +55,9 @@ filters = [
     {"file_name": os.path.join(output_directory, "Filtered_price_10_de.json"), "condition": lambda ticker: float(ticker.get("Price Change Today", 0)) < -10, "field": "Price Change Today"},
     {"file_name": os.path.join(output_directory, "Filtered_price_15_de.json"), "condition": lambda ticker: float(ticker.get("Price Change Today", 0)) < -20, "field": "Price Change Today"},
     {"file_name": os.path.join(output_directory, "Filtered_price_20_de.json"), "condition": lambda ticker: float(ticker.get("Price Change Today", 0)) < -30, "field": "Price Change Today"},
-    {"file_name": os.path.join(output_directory, "Filtered_DVSA_50.json"), "condition": lambda ticker: float(ticker.get("Price Change Today", 0)) > 50, "field": "Price Change Today"},
-    {"file_name": os.path.join(output_directory, "Filtered_DVSA_100.json"), "condition": lambda ticker: float(ticker.get("Price Change Today", 0)) > 100, "field": "Price Change Today"},
-    {"file_name": os.path.join(output_directory, "Filtered_DVSA_150.json"), "condition": lambda ticker: float(ticker.get("Price Change Today", 0)) > 150, "field": "Price Change Today"},
+    {"file_name": os.path.join(output_directory, "Filtered_DVSA_50.json"), "condition": lambda ticker: float(ticker.get("Volume Today Over Shares Available", 0)) > 50, "field": "Volume Today Over Shares Available"},
+    {"file_name": os.path.join(output_directory, "Filtered_DVSA_100.json"), "condition": lambda ticker: float(ticker.get("Volume Today Over Shares Available", 0)) > 100, "field": "Volume Today Over Shares Available"},
+    {"file_name": os.path.join(output_directory, "Filtered_DVSA_150.json"), "condition": lambda ticker: float(ticker.get("Volume Today Over Shares Available", 0)) > 150, "field": "Volume Today Over Shares Available"},
 ]
 
 def reset_filtered_files():
