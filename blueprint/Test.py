8e1d213734523854276d043765925b0206175d2a
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-01-30 08:17:22 -0600

    Update Test.py

--- blueprint/Test.py
+++ blueprint/Test.py
@@ -79,7 +79,7 @@ def extract_articles():
         paragraph_tag = article.select_one("p")  # Extract first paragraph
         time_tag = article.select_one('meta[itemprop="datePublished"]')
         
-        if headline_tag is "Null" or link_tag is "Null":  
+        if headline_tag == "Null" or link_tag == "Null":  
             continue  # Skip invalid articles
         headline = headline_tag.text.strip()
         link = f"https://finance.yahoo.com{link_tag['href']}" if 'href' in link_tag.attrs else None
