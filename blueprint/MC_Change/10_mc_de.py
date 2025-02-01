92f9d416dd5f9008b7beeb8c193a86b126f0e629
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-01-31 20:33:45 -0600

    Update and rename email_app_10_mc_de.py to 10_mc_de.py

--- blueprint/MC_Change/10_mc_de.py
+++ blueprint/MC_Change/10_mc_de.py
@@ -19,7 +19,7 @@ SENDER_EMAIL = 'noreply.rts@retailtradescanner.com'
 SENDER_PASSWORD = 'pIqvin-persi2-pibsij'
 
 # Paths to JSON files
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = r"/home/ec2-user/Stock-Scanner-Project/json/"
 JSON_FILE = os.path.join(BASE_DIR, 'json', '10_mc_de.json')
 STOCK_INFO_FILE = os.path.join(BASE_DIR, 'json', 'Filtered_market_cap_10_de.json')
 USED_TICKERS_FILE = os.path.join(BASE_DIR, 'json', 'ut_mc_10_de.json')
