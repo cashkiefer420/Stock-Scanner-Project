252d66cc0e4f1265f3ac4254af823d6b55ce78b5
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-01-31 20:46:04 -0600

    Update and rename email_app_10_pe_de.py to 10_pe_de.py

--- blueprint/PE_Change/10_pe_de.py
+++ blueprint/PE_Change/10_pe_de.py
@@ -19,7 +19,7 @@ SENDER_EMAIL = 'noreply.rts@retailtradescanner.com'
 SENDER_PASSWORD = 'pIqvin-persi2-pibsij'
 
 # Paths to JSON files
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = r"/home/ec2-user/Stock-Scanner-Project/"
 JSON_FILE = os.path.join(BASE_DIR, 'json', '10_pe_de.json')
 STOCK_INFO_FILE = os.path.join(BASE_DIR, 'json', 'Filtered_pe_10_de.json')
 USED_TICKERS_FILE = os.path.join(BASE_DIR, 'json', 'ut_pe_1o_de.json')
