4780e31606d9603fd12cd74c732488eb23ad0686
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-02-13 12:36:23 -0600

    Update 10_price_de_email.py

--- blueprint/Price_de/10_price_de_email.py
+++ blueprint/Price_de/10_price_de_email.py
@@ -12,7 +12,7 @@ import threading
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 SENDER_EMAIL = 'noreply.retailtradescanner@gmail.com'
-SENDER_PASSWORD = 'mzqmvhsjqeqrjmjv'
+APP_PASSWORD = 'mzqmvhsjqeqrjmjv'
 
 # Paths to JSON files
 BASE_DIR = r"/home/ec2-user/Stock-Scanner-Project/"
@@ -82,7 +82,7 @@ def send_stock_notifications():
         # Initialize SMTP server
         server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
         server.starttls()
-        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.login(SENDER_EMAIL, APP_PASSWORD)
 
         template = Template(html_template)
 
