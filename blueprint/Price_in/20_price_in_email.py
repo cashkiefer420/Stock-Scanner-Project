b9246f01b55961a91f58298faef6e23ab92029ee
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-02-27 07:56:18 -0600

    Update 20_price_in_email.py

--- blueprint/Price_in/20_price_in_email.py
+++ blueprint/Price_in/20_price_in_email.py
@@ -93,7 +93,7 @@ html_template = """
   <body>
     <div class="email-container">
       <div class="header">
-        Stock Alert: {{ stock_symbol }} Down 10%
+        Stock Alert: {{ stock_symbol }} Price Decrease 2o%
       </div>
       <div class="content">
         <p>Dear Investor,</p>
