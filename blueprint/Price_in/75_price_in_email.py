0c959eec31937b59df0987cc2690c052aa39da6d
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-02-27 08:07:15 -0600

    Update 75_price_in_email.py

--- blueprint/Price_in/75_price_in_email.py
+++ blueprint/Price_in/75_price_in_email.py
@@ -93,7 +93,7 @@ html_template = """
   <body>
     <div class="email-container">
       <div class="header">
-        Stock Alert: {{ stock_symbol }} Down 10%
+        Stock Alert: {{ stock_symbol }} Price Increase 75%
       </div>
       <div class="content">
         <p>Dear Investor,</p>
