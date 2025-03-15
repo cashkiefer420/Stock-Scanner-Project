400bc495fe33d81077d7d906e4cc543945dfb34c
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-03-15 09:14:58 -0500

    Add files via upload

---
+++ blueprint/News_display.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, jsonify
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    news_file_path = r'C:\Users\Carte\Documents\Stock-Scanner-Project-Windows\json\news.json'
+    with open(news_file_path, 'r') as file:
+        articles = json.load(file)
+    return render_template('News_display.html', articles=articles)
+
+@app.route('/articles', methods=['GET'])
+def get_articles():
+    news_file_path = r'C:\Users\Carte\Documents\Stock-Scanner-Project-Windows\json\news.json'
+    with open(news_file_path, 'r') as file:
+        articles = json.load(file)
+    return jsonify(articles)
+
+if __name__ == '__main__':
+    app.run(debug=True)
