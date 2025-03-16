75fcc05b964ea1c681a7ad584bae70d5d98f9aca
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-03-16 14:55:15 -0500

    Update 20_pe_de_flask.py

--- blueprint/PE_Change/20_pe_de_flask.py
+++ blueprint/PE_Change/20_pe_de_flask.py
@@ -72,4 +72,4 @@ def subscribe_email():
         return jsonify({"message": f"An error occurred: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5011)
