3af656800e95a24bf7132e9fdfa4dcaba6553c99
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-03-17 12:32:37 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Activate virtual environment (adjust the path as needed)
@@ -10,69 +9,69 @@ mkdir -p "$LOG_DIR"
 
 # Declare non-email Flask apps and their ports
 declare -A apps=(
-    ["50_DVSA_flask.py"]=5001
-    ["100_DVSA_flask.py"]=5002
-    ["150_DVSA_flask.py"]=5003
-    ["10_mc_de_flask.py"]=5004
-    ["10_mc_in_flask.py"]=5005
-    ["20_mc_de_flask.py"]=5006
-    ["20_mc_in_flask.py"]=5007
-    ["30_mc_de_flask.py"]=5008
-    ["30_mc_in_flask.py"]=5009
-    ["10_pe_de_flask.py"]=5010
-    ["10_pe_in_flask.py"]=5011
-    ["20_pe_de_flask.py"]=5012
-    ["20_pe_in_flask.py"]=5013
-    ["30_pe_de_flask.py"]=5014
-    ["30_pe_in_flask.py"]=5015
-    ["10_price_de_flask.py"]=5016
-    ["15_Price_de_flask.py"]=5017
-    ["20_price_de_flask.py"]=5018
-    ["20_price_in_flask.py"]=5019
-    ["50_price_in_flask.py"]=5020
-    ["75_price_in_flask.py"]=5021
-    ["1.125_volume_flask.py"]=5022
-    ["1.25_volume_flask.py"]=5023
-    ["1.5_volume_flask.py"]=5024
-    ["1.75_volume_flask.py"]=5025
-    ["2_volume_flask.py"]=5026
-    ["2.5_volume_flask.py"]=5027
-    ["3_volume_flask.py"]=5028
-    ["5x_volume_flask.py"]=5029
+    ["DVSA/50_DVSA_flask.py"]=5001
+    ["DVSA/100_DVSA_flask.py"]=5002
+    ["DVSA/150_DVSA_flask.py"]=5003
+    ["MC_Change/10_mc_de_flask.py"]=5004
+    ["MC_Change/10_mc_in_flask.py"]=5005
+    ["MC_Change/20_mc_de_flask.py"]=5006
+    ["MC_Change/20_mc_in_flask.py"]=5007
+    ["MC_Change/30_mc_de_flask.py"]=5008
+    ["MC_Change/30_mc_in_flask.py"]=5009
+    ["PE_Change/10_pe_de_flask.py"]=5010
+    ["PE_Change/10_pe_in_flask.py"]=5011
+    ["PE_Change/20_pe_de_flask.py"]=5012
+    ["PE_Change/20_pe_in_flask.py"]=5013
+    ["PE_Change/30_pe_de_flask.py"]=5014
+    ["PE_Change/30_pe_in_flask.py"]=5015
+    ["Price_de/10_price_de_flask.py"]=5016
+    ["Price_de/15_Price_de_flask.py"]=5017
+    ["Price_de/20_price_de_flask.py"]=5018
+    ["Price_in/20_price_in_flask.py"]=5019
+    ["Price_in/50_price_in_flask.py"]=5020
+    ["Price_in/75_price_in_flask.py"]=5021
+    ["Volume/1.125_volume_flask.py"]=5022
+    ["Volume/1.25_volume_flask.py"]=5023
+    ["Volume/1.5_volume_flask.py"]=5024
+    ["Volume/1.75_volume_flask.py"]=5025
+    ["Volume/2_volume_flask.py"]=5026
+    ["Volume/2.5_volume_flask.py"]=5027
+    ["Volume/3_volume_flask.py"]=5028
+    ["Volume/5x_volume_flask.py"]=5029
 )
 
 # Declare email-related scripts (including Email_filter.py) and their ports.
 # These scripts will only run between 8 AM and 5 AM (New York time).
 declare -A email_apps=(
-    ["50_DVSA_email.py"]=5101
-    ["100_DVSA_email.py"]=5102
-    ["150_DVSA_email.py"]=5103
-    ["10_mc_de_email.py"]=5104
-    ["10_mc_in_email.py"]=5105
-    ["20_mc_de_email.py"]=5106
-    ["20_mc_in_email.py"]=5107
-    ["30_mc_de_email.py"]=5108
-    ["30_mc_in_email.py"]=5109
-    ["10_pe_de_email.py"]=5110
-    ["10_pe_in_email.py"]=5111
-    ["20_pe_de_email.py"]=5112
-    ["20_pe_in_email.py"]=5113
-    ["30_pe_de_email.py"]=5114
-    ["30_pe_in_email.py"]=5115
-    ["10_price_de_email.py"]=5116
-    ["15_Price_de_email.py"]=5117
-    ["20_price_de_email.py"]=5118
-    ["20_price_in_email.py"]=5119
-    ["50_price_in_email.py"]=5120
-    ["75_price_in_email.py"]=5121
-    ["1.125_volume_email.py"]=5122
-    ["1.25_volume_email.py"]=5123
-    ["1.5_volume_email.py"]=5124
-    ["1.75_volume_email.py"]=5125
-    ["2_volume_email.py"]=5126
-    ["2.5_volume_email.py"]=5127
-    ["3_volume_email.py"]=5128
-    ["5_volume_email.py"]=5129
+    ["DVSA/50_DVSA_email.py"]=5101
+    ["DVSA/100_DVSA_email.py"]=5102
+    ["DVSA/150_DVSA_email.py"]=5103
+    ["MC_Change/10_mc_de_email.py"]=5104
+    ["MC_Change/10_mc_in_email.py"]=5105
+    ["MC_Change/20_mc_de_email.py"]=5106
+    ["MC_Change/20_mc_in_email.py"]=5107
+    ["MC_Change/30_mc_de_email.py"]=5108
+    ["MC_Change/30_mc_in_email.py"]=5109
+    ["PE_Change/10_pe_de_email.py"]=5110
+    ["PE_Change/10_pe_in_email.py"]=5111
+    ["PE_Change/20_pe_de_email.py"]=5112
+    ["PE_Change/20_pe_in_email.py"]=5113
+    ["PE_Change/30_pe_de_email.py"]=5114
+    ["PE_Change/30_pe_in_email.py"]=5115
+    ["Price_de/10_price_de_email.py"]=5116
+    ["Price_de/15_Price_de_email.py"]=5117
+    ["Price_de/20_price_de_email.py"]=5118
+    ["Price_in/20_price_in_email.py"]=5119
+    ["Price_in/50_price_in_email.py"]=5120
+    ["Price_in/75_price_in_email.py"]=5121
+    ["Volume/1.125_volume_email.py"]=5122
+    ["Volume/1.25_volume_email.py"]=5123
+    ["Volume/1.5_volume_email.py"]=5124
+    ["Volume/1.75_volume_email.py"]=5125
+    ["Volume/2_volume_email.py"]=5126
+    ["Volume/2.5_volume_email.py"]=5127
+    ["Volume/3_volume_email.py"]=5128
+    ["Volume/5_volume_email.py"]=5129
     ["Email_filter.py"]=5130
 )
 
