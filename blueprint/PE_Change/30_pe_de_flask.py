96d9100871a86acb4e7aabd264f45e3fc0d0e589
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-04-09 12:31:58 -0500

    Update 30_pe_de_flask.py

--- blueprint/PE_Change/30_pe_de_flask.py
+++ blueprint/PE_Change/30_pe_de_flask.py
@@ -11,7 +11,7 @@ def favicon():
 
 @app.route('/')
 def index():
-    return render_template('Index-pe-de-30.html')
+    return render_template('Index-Pe-de-30.html')
 
 # Paths to JSON files
 BASE_DIR = r"/home/ec2-user/Stock-Scanner-Project/"
