9712ff521a37c60229e59ab4ba2b4c9427df8edd
Toasterfire-come <carter.kiefer2010@outlook.com>
2025-04-30 14:10:11 -0500

    Update Download_S3.py

--- Download_S3.py
+++ Download_S3.py
@@ -5,6 +5,8 @@ import botocore
 S3_BUCKET = "exportbucket--use2-az1--x-s3"
 REGION = "us-east-2"
 EXPORT_KEY = "stock_data_export.json"
+PE_KEY = "PE_num.json"  # Key for PE data
+MARKETCAP_KEY = "MC_num.json"  # Key for Market Cap data
 
 # Initialize the S3 client
 s3_client = boto3.client("s3", region_name=REGION)
@@ -21,8 +23,12 @@ def fetch_file_from_s3(bucket_name, key, download_path):
         else:
             print(f"An error occurred: {e}")
 
-# Specify the local path to save the downloaded file
-local_file_path = "./json/stock_data_export.json"
+# Specify the local paths to save the downloaded files
+local_export_path = "./json/stock_data_export.json"
+local_pe_path = "./json/PE_num.json"
+local_marketcap_path = "./json/MC_num.json"
 
-# Fetch the file
-fetch_file_from_s3(S3_BUCKET, EXPORT_KEY, local_file_path)
+# Fetch the files
+fetch_file_from_s3(S3_BUCKET, EXPORT_KEY, local_export_path)
+fetch_file_from_s3(S3_BUCKET, PE_KEY, local_pe_path)
+fetch_file_from_s3(S3_BUCKET, MARKETCAP_KEY, local_marketcap_path)
